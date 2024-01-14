fdff97e64b725bf13b4bba1f6c0ff6362bbe74b2
Ali Bahadori <ali.bahadori41@yahoo.com>
2024-01-14 14:32:15 +0330

    Add transaction table and active,de-active & install method

---
+++ cf7_zarinpal.php
@@ -0,0 +1,158 @@
+<?php
+/*
+Plugin Name: Zarinpal Contact form 7 payment
+Plugin URI: https://github.com/AliBahadori41/cf7-zarinpal
+Description: اتصال فرم های Contact Form 7 به درگاه پرداخت ZarinPal
+Author: Ali Bahadori
+Author URI: https://bahadori.dev
+Version: 1.0
+*/
+
+if (!defined('ABSPATH')) exit;
+
+register_activation_hook(__FILE__, "cf7ZarinpalActivate");
+register_deactivation_hook(__FILE__, "cf7_zarinpal_deactivate");
+register_uninstall_hook(__FILE__, "cf7_zarinpal_uninstall");
+
+
+/**
+ * Active plugin
+ */
+function cf7ZarinpalActivate()
+{
+	global $wpdb;
+	
+    $table_name = $wpdb->prefix . "cf7_zarinpal_transactions";
+
+    $sql = "CREATE TABLE IF NOT EXISTS " . $table_name . " (
+        id mediumint(11) NOT NULL AUTO_INCREMENT,
+        idform bigint(11) DEFAULT '0' NOT NULL,
+        transaction_authority VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+        transaction_reference VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NULLABLE,
+        gateway VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+        cost bigint(11) DEFAULT '0' NOT NULL,
+        created_at bigint(11) DEFAULT '0' NOT NULL,
+        email VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci  NULL,
+        description VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+        user_mobile VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci  NULL,
+        status VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+        PRIMARY KEY id (id)
+    );";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+    dbDelta($sql);
+
+    // remove ajax from contact form 7 to allow for php redirects
+    function wp_config_put($slash = '')
+    {
+        $config = file_get_contents(ABSPATH . "wp-config.php");
+        $config = preg_replace("/^([\r\n\t ]*)(\<\?)(php)?/i", "<?php define('WPCF7_LOAD_JS', false);", $config);
+        file_put_contents(ABSPATH . $slash . "wp-config.php", $config);
+    }
+
+    if (file_exists(ABSPATH . "wp-config.php") && is_writable(ABSPATH . "wp-config.php")) {
+        wp_config_put();
+    } else if (file_exists(dirname(ABSPATH) . "/wp-config.php") && is_writable(dirname(ABSPATH) . "/wp-config.php")) {
+        wp_config_put('/');
+    } else {
+        ?>
+        <div class="error">
+            <p><?php _e('wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been activated.', 'cf7pp'); ?></p>
+        </div>
+        <?php
+        exit;
+    }
+
+    $cf7_zarinpal_options = array(
+        'merchant_id' => '',
+        'callback_url' => '',
+        'currency' => 'IRR'
+    );
+
+    add_option("cf7_zarinpal", $cf7_zarinpal_options);
+}
+
+
+/**
+ * De-Active plugin
+ */
+function cf7_zarinpal_deactivate()
+{
+
+    function wp_config_delete($slash = '')
+    {
+        $config = file_get_contents(ABSPATH . "wp-config.php");
+        $config = preg_replace("/( ?)(define)( ?)(\()( ?)(['\"])WPCF7_LOAD_JS(['\"])( ?)(,)( ?)(0|1|true|false)( ?)(\))( ?);/i", "", $config);
+        file_put_contents(ABSPATH . $slash . "wp-config.php", $config);
+    }
+
+    if (file_exists(ABSPATH . "wp-config.php") && is_writable(ABSPATH . "wp-config.php")) {
+        wp_config_delete();
+    } else if (file_exists(dirname(ABSPATH) . "/wp-config.php") && is_writable(dirname(ABSPATH) . "/wp-config.php")) {
+        wp_config_delete('/');
+    } else if (file_exists(ABSPATH . "wp-config.php") && !is_writable(ABSPATH . "wp-config.php")) {
+        ?>
+        <div class="error">
+            <p><?php _e('wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp'); ?></p>
+        </div>
+        <button onclick="goBack()">Go Back and try again</button>
+        <script>
+            function goBack() {
+                window.history.back();
+            }
+        </script>
+        <?php
+        exit;
+    } else if (file_exists(dirname(ABSPATH) . "/wp-config.php") && !is_writable(dirname(ABSPATH) . "/wp-config.php")) {
+        ?>
+        <div class="error">
+            <p><?php _e('wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp'); ?></p>
+        </div>
+        <button onclick="goBack()">Go Back and try again</button>
+        <script>
+            function goBack() {
+                window.history.back();
+            }
+        </script>
+        <?php
+        exit;
+    } else {
+        ?>
+        <div class="error">
+            <p><?php _e('wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp'); ?></p>
+        </div>
+        <button onclick="goBack()">Go Back and try again</button>
+        <script>
+            function goBack() {
+                window.history.back();
+            }
+        </script>
+        <?php
+        exit;
+    }
+    
+  
+    delete_option("cf7_zarinpal");
+    delete_option("cf7_zarinpal_my_plugin_notice_shown");
+
+}
+
+
+/**
+ * Uninstall plguin
+ */
+function cf7_zarinpal_uninstall()
+{
+}
+
+// display activation notice
+add_action('admin_notices', 'cf7pp_my_plugin_admin_notices');
+
+function cf7pp_my_plugin_admin_notices() {
+    if (!get_option('cf7pp_my_plugin_notice_shown')) {
+        echo "<div class='updated'><p><a href='admin.php?page=cf7pp_admin_table'>برای تنظیم اطلاعات درگاه  کلیک کنید</a>.</p></div>";
+        update_option("cf7pp_my_plugin_notice_shown", "true");
+    }
+}
+
